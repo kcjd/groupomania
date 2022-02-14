7923c63b8406cda656aae61a80c12280c744035c
Kevin Colonjard <kevincj@outlook.fr>
2022-02-14 15:07:40 +0100

    🏷️  add auth interfaces

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,9 +1,18 @@
-export interface LoginFields {
+export interface UserData {
+  id: number
+  lastname: string
+  firstname: string
+  position?: string
+  picture?: string
+  role: 'user' | 'moderator'
+}
+
+export interface LoginValues {
   email: string
   password: string
 }
 
-export interface SignupFields extends LoginFields {
+export interface SignupValues extends LoginValues {
   lastname: string
   firstname: string
 }

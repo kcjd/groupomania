2ab1e2e2d132a599453fd39881ddd0e5a4b20e84
Kevin Colonjard <kevincj@outlook.fr>
2022-02-17 17:27:36 +0100

    ✨ add users page

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,23 @@
+import { Link as RouterLink } from 'react-router-dom'
+
+import { Link } from '@chakra-ui/react'
+
+import { UserData } from '../types/types'
+import Card from './UI/Card'
+import User from './User'
+
+interface Props {
+  user: UserData
+}
+
+const UserCard = ({ user }: Props) => {
+  return (
+    <Link as={RouterLink} to={`/profile/${user.id}`}>
+      <Card as="article">
+        <User user={user} />
+      </Card>
+    </Link>
+  )
+}
+
+export default UserCard
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,5 +1,25 @@
+import { Heading, SimpleGrid } from '@chakra-ui/react'
+
+import { useAppSelector } from '../app/hooks'
+import UserCard from '../components/UserCard'
+import { selectAllUsers } from '../features/usersSlice'
+
 const Users = () => {
-  return <h1>Users</h1>
+  const users = useAppSelector(selectAllUsers)
+
+  return (
+    <>
+      <Heading as="h1" fontSize="md" fontWeight="semibold" mb={6} color="gray.600">
+        Tous les utilisateurs
+      </Heading>
+
+      <SimpleGrid columns={[1, 2]} gap={4}>
+        {users.map((user) => (
+          <UserCard user={user} key={user.id} />
+        ))}
+      </SimpleGrid>
+    </>
+  )
 }
 
 export default Users

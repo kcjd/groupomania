4af122ea66204b18b6a9227036b3504f13d33bcc
Kevin Colonjard <kevincj@outlook.fr>
2022-02-17 18:11:41 +0100

    💄 update app layout

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,18 +1,18 @@
 import { Outlet } from 'react-router-dom'
 
-import { Container } from '@chakra-ui/react'
+import { Container, Grid } from '@chakra-ui/react'
 
 import Header from './Header/Header'
 
 const AppLayout = () => {
   return (
-    <>
+    <Grid templateRows="auto 1fr" minH="100vh">
       <Header />
 
       <Container as="main" maxW="container.md" py={8}>
         <Outlet />
       </Container>
-    </>
+    </Grid>
   )
 }
 
